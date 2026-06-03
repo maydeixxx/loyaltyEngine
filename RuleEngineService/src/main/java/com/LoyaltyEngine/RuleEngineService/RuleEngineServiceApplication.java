5b68740322dfaa55952caa68a9daf243ae302592
Alex Karev <kruki999@yandex.ru>
2026-06-03 18:42:27 +0700

    init commit for ruleEngineService

---
+++ RuleEngineService/src/main/java/com/LoyaltyEngine/RuleEngineService/RuleEngineServiceApplication.java
@@ -0,0 +1,13 @@
+package com.LoyaltyEngine.RuleEngineService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuleEngineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuleEngineServiceApplication.class, args);
+	}
+
+}
---
+++ RuleEngineService/src/test/java/com/LoyaltyEngine/RuleEngineService/RuleEngineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LoyaltyEngine.RuleEngineService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleEngineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
